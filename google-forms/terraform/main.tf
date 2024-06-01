af93d0abf80dcc46c9218f5df98e8913763f3a40
Tshego-MM <132884419+Tshego-MM@users.noreply.github.com>
2024-06-01 10:44:53 +0200

    database security group and flyway (#15)
    * database security group
    
    * flyway
    
    * push to main

--- google-forms/terraform/main.tf
+++ google-forms/terraform/main.tf
@@ -50,8 +50,8 @@ resource "aws_security_group" "database_sg" {
   description = "Postgresql security group"
 
   ingress {
-    from_port   = 1433
-    to_port     = 1433
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
