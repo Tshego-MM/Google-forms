cffaf4952de3e0d367a3c5de2cbabe0bee5bb224
Tshego-MM <tshegofatso.mohlala@bbd.co.za>
2024-06-01 11:02:15 +0200

    set ssl to false

--- google-forms/backend/database/dbinnit.js
+++ google-forms/backend/database/dbinnit.js
@@ -7,7 +7,7 @@ const pool= new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
-    ssl: true
+    ssl: false
 })
 
 pool.connect((err, client, release) => {
