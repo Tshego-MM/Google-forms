b9426c4986d6738d3e3e13381c5216023fecdfc1
Vutlhari Mashimbyi <Vutlhari.Mashimbyi@bbd.co.za>
2024-06-01 20:39:28 +0200

    Accept only https requests

--- google-forms/backend/app.js
+++ google-forms/backend/app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(ensureSecure);
+app.use(ensureSecure);//comment this line in dev
 app.use(rateLimit);
 
 app.use('/api/', indexRouter);
