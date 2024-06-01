a0fe1e073548340f8f0c20d60850e3540044b16c
Tshego-MM <tshegofatso.mohlala@bbd.co.za>
2024-06-01 10:57:18 +0200

    ssl

--- google-forms/backend/database/dbinnit.js
+++ google-forms/backend/database/dbinnit.js
@@ -6,7 +6,8 @@ const pool= new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    ssl: true
 })
 
 pool.connect((err, client, release) => {
