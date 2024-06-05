7e80219a61e0ad048a3d188e522dd1e51d276c22
Vutlhari Mashimbyi <Vutlhari.Mashimbyi@bbd.co.za>
2024-06-05 19:41:19 +0200

    Added logger, cleaned up the code and survey_link

--- google-forms/backend/routes/userRoute.js
+++ google-forms/backend/routes/userRoute.js
@@ -1,26 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel');
-
-router.post('/', async (req, res) => {
-  try {
-    const userId = await User.createUser(req.body);
-    res.json({ message: `User created with ID: ${userId}` });
-  } catch (error) {
-    res.status(500).json({ message: `Error creating user : ${error}` });
-  }
-});
-//Commented out becuase its blocking other end points
-// router.get('/:userId', async (req, res) => {
-//   try {
-//     const userId = await User.getUserById(req.params.userId);
-//     res.json({ userId, });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Error getting user' });
-//   } 
-// });
-
 
 router.get("/testJWT", async(req,res) =>{
   res.send(200);
