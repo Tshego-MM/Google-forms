60afdf04c27ef86b3c83514c53db1025e7a0d8c4
Sphesihle Ntshingila <smklsw@proton.me>
2024-06-06 01:59:45 +0200

    fix: authentication (#29)

--- google-forms/src/app/pages/builder/builder.component.ts
+++ google-forms/src/app/pages/builder/builder.component.ts
@@ -92,7 +92,7 @@ export default class BuilderPage {
 
   save () {
     const form = {
-      tile: this.title,
+      title: this.title,
       description: this.description,
       questions: this.fields.map(f => ({
         required: f.properties.value.required,
@@ -100,15 +100,7 @@ export default class BuilderPage {
         questionType: 2
       }))
     }
-    // localStorage.setItem('FORM', JSON.stringify({
-    //   tile: this.title,
-    //   description: this.description,
-    //   // questions: this.fields.map(f => ({
-    //   //   ...f,
-    //   //   properties: f.properties.value
-    //   // })),
-    //   questions: 
-    // }))
+
     localStorage.setItem('FORM', JSON.stringify(form))
 
     this.formService
@@ -126,7 +118,7 @@ export default class BuilderPage {
             message: 'Form created successully!',
             config: { panelClass: 'info-notification' }
           })
-          this.router.navigateByUrl('/survey')
+          this.router.navigateByUrl('/')
         })
       )
       .subscribe()
--- google-forms/src/app/pages/home/home.component.ts
+++ google-forms/src/app/pages/home/home.component.ts
@@ -4,10 +4,10 @@ import { MatDialog } from "@angular/material/dialog";
 import FooterComponent from "@/components/footer/footer.component";
 import HeaderComponent from "@/components/header/header.component";
 import BuildFormDialog from "@/components/dialog/build-form/build-form.dialog.component";
-import { Router } from "@angular/router";
+import { ActivatedRoute, ActivationEnd, Router } from "@angular/router";
 import AuthService from "@/services/auth.service";
 import { getHashParameters } from "@/utilities/url";
-import { catchError, finalize, of, throwError } from "rxjs";
+import { catchError, finalize, of, pipe, throwError } from "rxjs";
 import SnackbarService from "@/services/snackbar.service";
 import FormService from "@/services/form.service";
 import { MatCardModule } from "@angular/material/card";
@@ -33,6 +33,7 @@ export default class HomePage implements OnInit {
   private readonly authService = inject(AuthService)
   private readonly formService = inject(FormService)
   private readonly snackbarService = inject(SnackbarService)
+  private readonly route = inject(ActivatedRoute)
 
   loggedIn = false
 
@@ -43,44 +44,15 @@ export default class HomePage implements OnInit {
   }
 
   ngOnInit () {
-    let isLocalStorageUpdated = false
-    let credentials = this.authService.fetchCredentials()
+    let credentials = getHashParameters()
 
-    const token = credentials?.['id_token']
-
-    if (token) {
-      this.authService
-        .verifyToken(credentials['token_type'], credentials['id_token'])
-        .pipe(
-          catchError(error => throwError(() => new Error(error.message)))
-        )
-        .subscribe(() => {
-          this.authService.persistCredentials(credentials)
-          isLocalStorageUpdated = true
-        })
+    if (credentials?.['id_token'] && credentials?.['token_type']) {
+      this.authService.persistCredentials(credentials)
+    } else {
+      credentials = this.authService.fetchCredentials()
     }
 
-    if (isLocalStorageUpdated) return
-
-    credentials = getHashParameters()
-
-    if (credentials) {
-      this.authService
-        .verifyToken(credentials['token_type'], credentials['id_token'])
-        .pipe(
-          catchError(error => {
-            this.router.navigateByUrl('/auth')
-            return throwError(() => new Error(error.message))
-          })
-        )
-        .subscribe(() => {
-          this.authService.persistCredentials(credentials)
-        })
-    }
-
-    this.formService.fetchForms().subscribe(forms => {
-      this.forms = forms
-    })
+    this.formService.fetchForms().subscribe(forms => this.forms = forms)
   }
 
   logout () {
